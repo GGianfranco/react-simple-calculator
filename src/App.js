c3509b175703ec14442e31a9b9bea8bbb8083877
GGianfranco <77826002+GGianfranco@users.noreply.github.com>
2022-02-17 14:44:44 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
+import { Button, Typography } from "@mui/material";
 
 function App() {
+  const [sum, setSum] = useState(0);
+  const [addend1, setAddend1] = useState(0);
+  const [addend2, setAddend2] = useState(0);
+
+  const updateAddend1 = (event) => setAddend1(Number(event.target.value));
+  const updateAddend2 = (event) => setAddend2(Number(event.target.value));
+
+  useEffect(() => {
+    setSum(Number(addend1) + Number(addend2));
+  }, [addend1, addend2]);
+
+  const reset = () => {
+    setAddend1(0);
+    setAddend2(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Box className="calculator">
+        <Box className="title">
+          <Typography noWrap variant="h2" component="div">
+            Simple Calculator
+          </Typography>
+        </Box>
+        <Box className="addition">
+          <TextField
+            className="addend"
+            id="addend1"
+            label="Addend"
+            value={addend1}
+            onChange={updateAddend1}
+            type="number"
+            size="medium"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <LocalHospitalIcon fontSize="large" />
+          <TextField
+            className="addend"
+            id="addend2"
+            label="Addend"
+            type="number"
+            value={addend2}
+            onChange={updateAddend2}
+            size="medium"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <TextField
+            className="sum"
+            disabled
+            id="standard-disabled"
+            label="Sum"
+            value={sum}
+            variant="filled"
+            size="medium"
+          />
+        </Box>
+        <Button className="reset" variant="contained" onClick={reset}>
+          Reset
+        </Button>
+      </Box>
     </div>
+    //   <div className="app">
+    //     <div className="calculator">
+    //       <input
+    //         type="number"
+    //         value={addend1}
+    //         onChange={updateAddend1}
+    //         className="addend"
+    //       />
+    //       <input
+    //         type="number"
+    //         value={addend2}
+    //         onChange={updateAddend2}
+    //         className="addend"
+    //       />
+    //       <div className="sum">{Number(addend1) + Number(addend2)}</div>
+    //     </div>
+    //   </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import "./fonts.css";
+
+const THEME = createTheme({
+  typography: {
+    fontFamily: `"Pangolin",cursive`,
+    fontWeightLight: 900,
+    fontWeightRegular: 900,
+    fontWeightMedium: 900,
+    fontWeightBold: 900,
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={THEME}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
